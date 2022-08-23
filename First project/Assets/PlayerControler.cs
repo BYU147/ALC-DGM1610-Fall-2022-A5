e3cfae25a858c11444482982c3295ddb74fdc7e6
mauricio <mauricio.p147@stu.nebo.edu>
2022-08-23 14:10:49 -0600

    Unity First Project

---
+++ First project/Assets/PlayerControler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public int speed;
+    public float hInput;
+    public float vInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      hInput = Input.GetAxis("Horizontal"); // left and right movment
+      vInput = Input.GetAxis("Vertical"); // forward and backward movment
+      
+      transform.Translate(Vector3.right * speed * hInput * Time.deltaTime); // move character left and right
+      
+      transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime); // move character forward and back
+    }
+}
