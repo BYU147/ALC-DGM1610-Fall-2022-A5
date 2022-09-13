34a14700e724ecbd5ceb9bae2312f6f0b39bdb04
mauricio <mauricio.p147@stu.nebo.edu>
2022-09-13 14:19:47 -0600

    made spawner

---
+++ baloon pop/Assets/SpawnManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] balloonPrefabs; // Array to store balloon game object
+    public float startDelay = 0.5f;
+    public float spawnInterval = 1.5f;
+    
+    //public int balloonIndex; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBalloon", startDelay, spawnInterval);
+    }
+    
+    void SpawnRandomBalloon()
+    {
+        // Get a random postion on the x-axis
+        Vector3 spawnPos = new Vector3(Random.Range(-5,5),10,0);
+        // Pick a random balloon from the balloon array
+        int balloonIndex = Random.Range(0,balloonPrefabs.Length);
+        // Spawn random balloon at spawn position
+        Instantiate(balloonPrefabs[balloonIndex], spawnPos, balloonPrefabs[balloonIndex].transform.rotation);
+    }
+    //update is called once per frame
+    void Update()
+    {
+    
+    }
+}
