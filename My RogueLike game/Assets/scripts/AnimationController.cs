214d41e37be995affb1c994178b7c41021e5b178
mauricio <mauricio.p147@stu.nebo.edu>
2022-11-04 14:20:36 -0600

    made animation start and stop

---
+++ My RogueLike game/Assets/scripts/AnimationController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPPro
+
+public class AnimationController : MonoBehaviour
+{
+    
+    public Animator animSpeed;
+    public Animation anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animSpeed = GetComponent<Animator>();
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+            PlayAnim();
+        
+        if(Input.GetKeyDown(KeyCode.E))
+            StopAnim();
+    }
+    
+    void PlayAnim()
+    {
+        animSpeed.speed = 1f;
+        Debug.Log("Animation is Playing");
+    }
+    
+    void StopAnim()
+    {
+        animSpeed.speed = 0f;
+        Debug.Log("Animation is paused");
+    }
+    
+    
+    public void PushPlay()
+    {
+        anim.Play();
+    }
+    
+    public void PushStop()
+    {
+        anim.Stop();
+    }
+}
